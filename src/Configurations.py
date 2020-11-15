4f92bd29eb1e420b1b6df24c8d8479779f331274
Andrei Gurguta <andrei_gurguta@yahoo.com>
2020-11-16 00:45:29 +0200

    implemented config changes

--- src/Configurations.py
+++ src/Configurations.py
@@ -4,7 +4,7 @@ import os
 class Configurations:
     """Object holding configurations for the project. Encapsulates reading configs from a fil"""
 
-    def __init__(self, config_file_path="./extra/config.txt"):
+    def __init__(self, config_file_path="../CONFIGS/lotopy/config.txt"):
         with open(config_file_path, mode="r", encoding="utf-8") as config_file:
             self.__config_dict = dict(arg.rstrip("\n").split("=") for arg in config_file.readlines())
 
